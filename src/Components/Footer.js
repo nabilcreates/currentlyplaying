b01cf156d2faf87b1f1fe4ec7a0a95319a35e4e1
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-05-02 04:05:11 +0800

    Footer change

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Footer(){
     return(
-        <h6>This is website shows you what song I am currently playing now on Spotify. This is a project that was created by <a href='https://github.com/renabil/' >me</a></h6>
+        <h6>This is website shows you what song I am currently playing now on Spotify. This project was created by <a href='https://github.com/renabil/' >me</a> (and it's open-sourced)</h6>
     )
 }
